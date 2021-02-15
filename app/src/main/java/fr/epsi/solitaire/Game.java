93f927a182c32246286a8af26c1536d4550b6a62
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-02-15 22:44:58 +0100

    Commit sur partie1 sur creation class Game

--- app/src/main/java/fr/epsi/solitaire/Game.java
+++ app/src/main/java/fr/epsi/solitaire/Game.java
@@ -1,4 +1,21 @@
 package fr.epsi.solitaire;
 
-public class Game {
+import java.io.Serializable;
+import java.util.Vector;
+
+
+public class Game implements Serializable {
+
+    public static class Stack extends java.util.Stack<Card> {}
+    public static class Deck extends java.util.Stack<Card> {}
+
+
+    public static final int STACK_COUNT = 4;
+    public static final int DECK_COUNT = 7;
+
+    public Stack [] stacks = new Stack[STACK_COUNT];
+    public Deck [] decks = new Deck[DECK_COUNT];
+    public Vector<Card> pioche = new Vector<>();
+    public Vector<Card> returnedPioche = new Vector();
+
 }
