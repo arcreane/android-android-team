a442e8df0294e6ef6e695b6a4c1ca89cede7e6aa
TThanushan <thanushantharmabalan@gmail.com>
2021-02-16 15:52:05 +0100

    image load method in GamView class done
    Loading cards sprite.

--- app/src/main/java/fr/epsi/solitaire/GameView.java
+++ app/src/main/java/fr/epsi/solitaire/GameView.java
@@ -2,15 +2,35 @@ package fr.epsi.solitaire;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class GameView extends View {
 
-    private int headerBackgroundColor;
-    private int headerForegroundColor;
-    private int backgroundColor;
-    private int redColor;
+    int headerBackgroundColor;
+    int headerForegroundColor;
+    int backgroundColor;
+    int redColor;
+
+    public Game game = new Game();
+
+    Bitmap imgPique;
+    Bitmap imgPiqueLittle;
+    Bitmap imgTreffle;
+    Bitmap imgTreffleLittle;
+    Bitmap imgCarreau;
+    Bitmap imgCarreauLittle;
+    Bitmap imgCoeur;
+    Bitmap imgCoeurLittle;
+
+    Bitmap imgBack;
+
+    float deckWidth;
+    float deckHeight;
+    float deckMargin;
 
 
     public GameView(Context context) {
@@ -23,12 +43,53 @@ public class GameView extends View {
         postConstruct();
     }
 
+    // Chargement des codes couleurs.
     private void postConstruct() {
         Resources res = getResources();
-        headerBackgroundColor = ((Resources) res).getColor( R.color.colorPrimaryDark );
+        headerBackgroundColor = res.getColor( R.color.colorPrimaryDark );
         headerForegroundColor = res.getColor( R.color.headerForegroundColor );
         backgroundColor = res.getColor( R.color.backgroundColor );
         redColor = res.getColor( R.color.redColor );
+    }
+
+
+    // Retaillage des bitmaps en fonction de la taille de la fenêtre.
+    @Override
+    protected void onSizeChanged( int width, int height, int oldw, int oldh ) {
+        super.onSizeChanged( width, height, oldw, oldh );
+
+        deckMargin = width * 0.025f;
+        deckWidth = (width - (Game.DECK_COUNT + 1) * deckMargin) / Game.DECK_COUNT;
+        deckHeight = deckWidth * 1.4f;
+
+        try {
+            int imageSize = (int) (deckWidth * 0.66);
+            int imageLittleSize = (int) (deckWidth / 3);
+
+
+            imgPique = BitmapFactory.decodeResource(getResources(), R.drawable.pique);
+            imgPiqueLittle = Bitmap.createScaledBitmap(imgPique, imageLittleSize, imageLittleSize, true);
+            imgPique = Bitmap.createScaledBitmap(imgPique, imageSize, imageSize, true);
+
+            imgTreffle = BitmapFactory.decodeResource(getResources(), R.drawable.treffle);
+            imgTreffleLittle = Bitmap.createScaledBitmap(imgTreffle, imageLittleSize, imageLittleSize, true);
+            imgTreffle = Bitmap.createScaledBitmap(imgTreffle, imageSize, imageSize, true);
+
+            imgCoeur = BitmapFactory.decodeResource(getResources(), R.drawable.coeur);
+            imgCoeurLittle = Bitmap.createScaledBitmap(imgCoeur, imageLittleSize, imageLittleSize, true);
+            imgCoeur = Bitmap.createScaledBitmap(imgCoeur, imageSize, imageSize, true);
+
+            imgCarreau = BitmapFactory.decodeResource(getResources(), R.drawable.carreau);
+            imgCarreauLittle = Bitmap.createScaledBitmap(imgCarreau, imageLittleSize, imageLittleSize, true);
+            imgCarreau = Bitmap.createScaledBitmap(imgCarreau, imageSize, imageSize, true);
+
+            imgBack = BitmapFactory.decodeResource(getResources(), R.drawable.back);
+            imgBack = Bitmap.createScaledBitmap(imgBack, (int)deckWidth, (int)deckHeight, true);
+
+        } catch (Exception exception) {
+            Log.e("ERROR", "Error loading card images");
+        }
+
     }
 
 
