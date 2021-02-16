a577e042772c4a03a85fb3c663db8d6f516f336f
mikebita23 <mikesbita@gmail.com>
2021-02-16 10:19:21 +0100

    Partie2 Début de l'état initiale du jeu constructeur

--- app/src/main/java/fr/epsi/solitaire/Game.java
+++ app/src/main/java/fr/epsi/solitaire/Game.java
@@ -18,4 +18,46 @@ public class Game implements Serializable {
     public Vector<Card> pioche = new Vector<>();
     public Vector<Card> returnedPioche = new Vector();
 
+    // ... Début de l'état initiale du jeu constructeur...
+
+    public Game() {
+
+        //   On instanci toute les cartes du jeu
+        for( int i=1; i<=13; i++ ) {
+            pioche.add( new Card( Card.CardType.CARREAU, i ) );
+            pioche.add( new Card( Card.CardType.COEUR, i ) );
+            pioche.add( new Card( Card.CardType.PIQUE, i ) );
+            pioche.add( new Card( Card.CardType.TREFLE, i ) );
+        }
+
+        //  On fait le mélange des cartes
+        for( int round=0; round<200; round++ ) {
+            int position = (int) ( Math.random() * pioche.size() );
+            Card removedCard = pioche.elementAt( position );
+            pioche.removeElementAt( position );
+            pioche.add( removedCard );
+        }
+
+        // Ici il y a sept decks avec des cartes tirées de manière aléatoirement dans la pioche du jeu
+        for( int deckIndex=0; deckIndex<DECK_COUNT; deckIndex++ ) {
+            decks[deckIndex] = new Deck();
+            for( int cardIndex = 0; cardIndex < deckIndex+1; cardIndex++ ) {
+                int position = (int) ( Math.random() * pioche.size() );
+                Card removedCard = pioche.elementAt( position );
+                pioche.removeElementAt( position );
+                decks[deckIndex].push( removedCard );
+                if ( cardIndex == deckIndex ) removedCard.setReturned( true );
+            }
+        }
+
+        // Puis ici on initialise les quatre stacks du jeu.
+        for( int stackIndex=0; stackIndex<STACK_COUNT; stackIndex++ ) {
+            stacks[stackIndex] = new Stack();
+        }
+
+    }
+
+
+
+
 }
