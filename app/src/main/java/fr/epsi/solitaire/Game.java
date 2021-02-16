5dbe1eb7a607f91e1b37720ac283999fd39bf164
mikebita23 <mikesbita@gmail.com>
2021-02-16 11:02:52 +0100

    Vérification avec conditions des déplacments des cartes dans une stacks et desks

--- app/src/main/java/fr/epsi/solitaire/Game.java
+++ app/src/main/java/fr/epsi/solitaire/Game.java
@@ -87,4 +87,66 @@ public class Game implements Serializable {
 
         return -1;
     }
+
+
+
+
+    /**
+     - Vérifie si une carte peut être déposée sur un des sept decks.
+      - Si lacard La carte à déposer.
+     - Si l'indice du deck sur lequel la carte peut être déposée,
+     - Et si verification -1 si ce n'est pas possible.
+     */
+    public int canMoveCardToDeck( Card card ) {
+        // Au cas où la carte est un roi et qu'un deck est vide, alors on peut poser la carte
+        if ( card.getValue() == 13 ) {
+            for( int deckIndex=0; deckIndex<DECK_COUNT; deckIndex++ ) {
+                if ( this.decks[deckIndex].isEmpty() ) return deckIndex;
+            }
+        }
+
+        // Est-ce que la carte peut être placée sur un deck ?
+        for( int deckIndex=0; deckIndex<DECK_COUNT; deckIndex++ ) {
+            Deck deck = this.decks[deckIndex];
+            if ( deck.size() > 0 ) {
+                if ( deck.lastElement().getColor() == card.getColor() ) continue;
+                if ( deck.lastElement().getValue() == card.getValue()+1 ) return deckIndex;
+            }
+        }
+
+        return -1;
+    }
+
+
+
+
+    /**
+     - On vérifie si le jeu est terminé, chaque stack doit avoir bien 13 cartes.
+     - C'est vrai si le jeu est terminé, false dans le cas contraire.
+     */
+    public boolean isFinish() {
+        return stacks[0].isEmpty() == false && stacks[0].lastElement().getValue() == 13 &&
+                stacks[1].isEmpty() == false && stacks[1].lastElement().getValue() == 13 &&
+                stacks[2].isEmpty() == false && stacks[2].lastElement().getValue() == 13 &&
+                stacks[3].isEmpty() == false && stacks[3].lastElement().getValue() == 13;
+    }
+
+
+
+    /**
+     - Ici on peut voir et savoir si toutes les cartes du jeu sont retournées et si qu'on peut lancer une terminaison automatique du jeu.
+     - C'est vrai si toutes les cartes sont retournées, false dans le cas contraire.
+     */
+    public boolean allIsReturned() {
+        for( int i=0; i<DECK_COUNT; i++ ) {
+            Deck deck = decks[i];
+            if ( deck.size() > 0 && deck.firstElement().isReturned() == false ) return false;
+        }
+        return true;
+    }
+
 }
+
+
+
+
