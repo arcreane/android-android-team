2b2243ef5290491f50a33ca0fbc43cb67d9635a7
TThanushan <thanushantharmabalan@gmail.com>
2021-02-19 18:29:27 +0100

    * copie des fichiers de la branche partie6 vers la branche master

---
+++ app/src/main/java/com/example/android_android_team/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android_android_team;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        gameView = findViewById( R.id.gameView );
+    }
+
+    @Override
+    protected void onPause() {
+    super.onPause();
+
+    }
+}
